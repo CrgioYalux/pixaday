a1e90186bd0e5b6e4535680f38a3a14e5bb5719a
Sergio Yalux <randaledomman1@gmail.com>
2023-02-24 01:06:27 -0300

    feat: create provider for color palette state and actions

---
+++ src/providers/ColorPalette/index.tsx
@@ -0,0 +1,38 @@
+import { createContext, useContext } from "react";
+import { useColorPalette } from "../../hooks/useColorPalette";
+import type { useColorPaletteState } from "../../hooks/useColorPalette";
+
+interface ColorPaletteProviderProps {
+    children: React.ReactNode;
+}
+
+type ColorPaletteContext = readonly [
+    colorPalette: useColorPaletteState[0],
+    color: useColorPaletteState[1],
+    actions: useColorPaletteState[2],
+];
+
+const ColorPaletteContext = createContext<ColorPaletteContext>([
+    [],
+    'white',
+    {
+        selectColor: () => {},
+        createColorPalette: () => {},
+    }
+]);
+
+export const useColorPaletteProvider = () => useContext<ColorPaletteContext>(ColorPaletteContext);
+
+const ColorPaletteProvider: React.FC<ColorPaletteProviderProps> = ({ children }) => {
+    const [colorPalette, color, actions] = useColorPalette();
+
+    const value = [colorPalette, color, actions] as const;
+
+    return (
+        <ColorPaletteContext.Provider value={value}>
+            {children}
+        </ColorPaletteContext.Provider>
+    );
+};
+
+export default ColorPaletteProvider;
