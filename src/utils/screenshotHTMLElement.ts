277fc9c9d562e8a8cbb9201e3c35e0e79f58b0af
Sergio Yalux <randaledomman1@gmail.com>
2023-03-12 03:33:12 -0300

    feat: create util that screenshots color matrix and downloads it

---
+++ src/utils/screenshotHTMLElement.ts
@@ -0,0 +1,13 @@
+import html2canvas from 'html2canvas';
+
+const screenshotHTMLElement = (elementID: string): void => {
+  const element: HTMLElement | null = document.getElementById(elementID);
+  if (element) html2canvas(element).then((canvas) => {
+    const link = document.createElement('a');
+    link.download = 'Screenshot.png';
+    link.href = canvas.toDataURL();
+    link.click();
+  })
+};
+
+export { screenshotHTMLElement };
