bf1a47bf3bdb5fbcfba7beeafce4524f120fb2e3
Sergio Yalux <randaledomman1@gmail.com>
2023-03-12 00:19:12 -0300

    feat: create component for switching colors selection

--- src/components/ColorPalette/index.tsx
+++ src/components/ColorPalette/index.tsx
@@ -4,11 +4,26 @@ import './ColorPalette.css';
 
 interface ColorPaletteProps {}
 
+const ColorPaletteModeSelect: React.FC<{}> = () => {
+    const [,, actions, colorsOrigin] = useColorPaletteProvider();
+
+    return (
+        <label className='ColorPaletteModeSelect'>
+            <span>Colors:</span>
+            <select onChange={() => actions.switchColorsOrigin()} defaultValue={colorsOrigin}>
+                <option value='today'>Today's selection</option>
+                <option value='random'>Random selection</option>
+            </select>
+        </label>
+    );
+}
+
 const ColorPalette: React.FC<ColorPaletteProps> = ({}) => {
     const [colorPalette, color, actions] = useColorPaletteProvider();
 
     return (
         <div className='ColorPalette'>
+            <ColorPaletteModeSelect />
             <div className='ColorPalette__list'>
             {colorPalette.map((c) => (
                 <div
