fc18789dea8596b6b96d77f038310d3bab645670
Sergio Yalux <randaledomman1@gmail.com>
2024-05-04 11:40:50 -0300

    feat: migrate matrix\'s size range tool to tailwind

--- src/components/ColorMatrixTools/MatrixSizeRange.tsx
+++ src/components/ColorMatrixTools/MatrixSizeRange.tsx
@@ -1,35 +1,83 @@
 import { useState } from 'react';
-import { useColorMatrixProvider } from "../../providers/ColorMatrix";
+import { useColorMatrixProvider } from '../../providers/ColorMatrix';
 
 import {
     COLOR_MATRIX_MIN_SIZE,
     COLOR_MATRIX_MAX_SIZE 
 } from '../../providers/ColorMatrix/consts';
 
-import './ColorMatrixTools.css';
-
 const MatrixSizeRange: React.FC<{}> = () => {
     const [matrixSize, setMatrixSize] = useState<number>(COLOR_MATRIX_MIN_SIZE);
     const [state, actions] = useColorMatrixProvider();
+    const [isMatrixSizeChanging, setIsMatrixSizeChanging] = useState<boolean>(false);
 
     return (
         <label
-        className='MatrixSizeRange'
-        htmlFor="matrix_size"
+        className='flex gap-2 p-4 py-2 items-center bg-brand-900 rounded-full'
+        htmlFor='matrix_size'
         >
-            <div>{state.colorMatrixSize.width}</div>
             <input
-            type="range"
-            id="matrix_size"
+            className='flex-1 peer'
+            type='range'
+            id='matrix_size'
             min={COLOR_MATRIX_MIN_SIZE}
             max={COLOR_MATRIX_MAX_SIZE}
-            onMouseUp={() => actions.colorMatrix.changeSize(matrixSize)}
-            onTouchEnd={() => actions.colorMatrix.changeSize(matrixSize)}
+            onMouseUp={() => {
+                actions.colorMatrix.changeSize(matrixSize);
+                setIsMatrixSizeChanging(false);
+            }}
+            onTouchEnd={() => {
+                actions.colorMatrix.changeSize(matrixSize);
+                setIsMatrixSizeChanging(false);
+            }}
             onChange={(event) => {
+                setIsMatrixSizeChanging(true);
                 setMatrixSize(Number(event.currentTarget.value));
             }}
             defaultValue={state.colorMatrixSize.width}
             />
+            <div 
+            className='peer-active:*:*:text-xl peer-active:has-[div[role="text"]]:text-base order-first flex-none grid place-items-center'
+            >
+                <div
+                className='relative w-8 flex flex-wrap text-brand-400 transition-colors duration-100'
+                role='icon'
+                >
+                    <div 
+                    className='absolute w-[2ch] grid place-items-center text-brand-100 top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%]'
+                    role='text'
+                    >
+                        {matrixSize}
+                    </div>
+                    <div
+                    className='w-4 h-4 border border-current'
+                    />
+                    <div
+                    className='w-4 h-4 border border-current'
+                    style={{
+                        borderStyle: isMatrixSizeChanging
+                            ? 'dashed'
+                            : 'solid'
+                    }}
+                    />
+                    <div
+                    className='w-4 h-4 border border-current'
+                    style={{
+                        borderStyle: isMatrixSizeChanging
+                            ? 'dashed'
+                            : 'solid'
+                    }}
+                    />
+                    <div
+                    className='w-4 h-4 border border-current border-dashed'
+                    style={{
+                        borderStyle: isMatrixSizeChanging
+                            ? 'dashed'
+                            : 'solid'
+                    }}
+                    />
+                </div>
+            </div>
         </label>
     );
 };
